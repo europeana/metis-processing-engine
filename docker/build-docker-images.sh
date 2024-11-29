383b7e05198303b519238b289d7a2670aee21f62
woz <pwozniak@man.poznan.pl>
2024-11-29 11:52:39 +0100

    MET-6224 k8s config modification

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -9,9 +9,5 @@ docker build --no-cache -t flink:1.20.0-java21_poc .
 cd ..
 cd flink-node
 echo building :: flink-node $(pwd)
-docker build --no-cache -t flink-node-poc .
-cd ..
-cd ..
-echo building :: flink-rich-node $(pwd)
-docker build --no-cache -t flink-rich-node-poc -f ./docker/flink-node-with-application/Dockerfile .
+docker build --no-cache -t metis-processing-engine-flink:1.20-java21 .
 
