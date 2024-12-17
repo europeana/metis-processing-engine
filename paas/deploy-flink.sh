e57ae453c32d47e50843de35a5d5b0650d2dc710
Ruufuus <rufusikx3@gmail.com>
2024-12-17 10:03:03 +0100

    MET-6296 Refactor directory structure Rename some files to unify formatting

--- paas/deploy-flink.sh
+++ paas/deploy-flink.sh
@@ -7,11 +7,11 @@ oc project | grep ecloud-flink-poc
 echo Project OK
 
 #Configure volumes
-oc apply -f deployments-volume.yaml
-oc apply -f web-upload-volume.yaml
-oc apply -f job-manager-working-volume.yaml
-oc apply -f job-manager-dumps-volume.yaml
-oc apply -f flink-data-volume.yaml
+oc apply -f flink/deployments-volume.yaml
+oc apply -f flink/web-upload-volume.yaml
+oc apply -f flink/job-manager-working-volume.yaml
+oc apply -f flink/job-manager-dumps-volume.yaml
+oc apply -f flink/flink-data-volume.yaml
 
 #Configure service account used by Flink to manipulate Kubernetes
 oc apply -f service-account/flink-sa-role.yaml
@@ -19,12 +19,12 @@ oc apply -f service-account/flink-sa.yaml
 oc apply -f service-account/flink-sa-rolebinding.yaml
 
 # Configuration and service definition
-oc apply -f flink-configuration-configmap.yaml
-oc apply -f jobmanager-service.yaml
+oc apply -f flink/flink-configuration-configmap.yaml
+oc apply -f flink/job-manager-service.yaml
 
 # Create the deployments for the cluster
-oc apply -f jobmanager-session-deployment.yaml
-oc apply -f taskmanager-session-deployment.yaml
+oc apply -f flink/job-manager-session-deployment.yaml
+oc apply -f flink/task-manager-session-deployment.yaml
 
 # Create client
-oc apply -f flink-client.yaml
+oc apply -f flink/flink-client.yaml
