fee09a902f936ce1e1a410ea5724c98320acbf48
woz <pwozniak@man.poznan.pl>
2025-02-04 14:19:59 +0100

    Reapply "MET-5947 flink version upgraded to 1.20"
    This reverts commit 714bcdc2e66e7b442781f1a21ea342186beebbda.

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -5,9 +5,9 @@ echo building flink-poc docker image that can be deployed on the openshift clust
 #images
 cd flink_java21
 echo building :: flink java 21 base $(pwd)
-docker build --no-cache -t flink:1.18.1-java21_poc .
+docker build --no-cache -t flink:1.20.0-java21_poc .
 cd ..
 cd flink-node
 echo building :: flink-node $(pwd)
-docker build --no-cache -t metis-processing-engine-flink:1.18.1-java21 .
+docker build --no-cache -t metis-processing-engine-flink:1.20.0-java21 .
 
