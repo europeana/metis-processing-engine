556b50fec86abbdcd0c441be1c0670617b51b0e4
marcin-rp <mratajczak@man.poznan.pl>
2025-02-13 16:17:05 +0100

    MET-6311 Moved postgresql library to the shared folder embedded into Flink node. Its driver caused memory leak.

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -3,11 +3,22 @@ set -e
 echo building flink-poc docker image that can be deployed on the openshift cluster.
 
 #images
-cd flink_java21
+cd flink-java21
 echo building :: flink java 21 base $(pwd)
 docker build --no-cache -t flink:1.20.0-java21_poc .
 cd ..
-cd flink-node
+
+cd flink-with-tools
+echo building :: flink-node $(pwd)
+docker build --no-cache -t flink-with-tools:1.20.0-java21 .
+cd ..
+
+cd metis-processing-engine-flink
 echo building :: flink-node $(pwd)
 docker build --no-cache -t metis-processing-engine-flink:1.20.0-java21 .
+cd ..
+
+
+#docker tag metis-processing-engine-flink:1.20.0-java21 registry.paas.psnc.pl/ecloud-poc/metis-processing-engine-flink:1.20.0-java21
+#docker push registry.paas.psnc.pl/ecloud-poc/metis-processing-engine-flink:1.20.0-java21
 
