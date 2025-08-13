57f404a6934018693dfca23f31fc9efb731adbef
woz <pwozniak@man.poznan.pl>
2025-08-13 09:20:48 +0200

    MET-6679 tests for RegularDbEnumerator

---
+++ test-commons/src/main/resources/testData.sql
@@ -0,0 +1,23 @@
+insert into "batch-framework".task_info(task_id, commit_count, write_count)
+values ('1', '1', '1');
+
+
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_1', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_2', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_3', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_4', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_5', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_6', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_7', 'data');
+insert into "batch-framework".execution_record(dataset_id, execution_id, execution_name, record_id, record_data)
+values ('datasetId', 'executionId', 'executionName', 'recordId_8', 'data');
+
+
+
